9c8891f90ee008563705241b783a39bb83865f13
David Merchan <juan1014166@gmail.com>
2021-08-17 18:18:37 -0500

    inject apiKey value in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,18 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 
+const API_KEY = '12345634';
+const API_KEY_PROD = 'production1234';
+
 @Module({
   imports: [ProductsModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+    },
+  ],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  constructor(@Inject('API_KEY') private apiKey: string) {}
+
   getHello(): string {
-    return 'Hello World!';
+    return `apiKey: ${this.apiKey}`;
   }
 }
