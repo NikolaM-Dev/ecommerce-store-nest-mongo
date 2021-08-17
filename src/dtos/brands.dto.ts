6753c59e9a4c626b95ebfaf657bdf44f8ac3d6c0
David Merchan <juan1014166@gmail.com>
2021-08-16 20:25:34 -0500

    correction is not empty in dtos

--- src/dtos/brands.dto.ts
+++ src/dtos/brands.dto.ts
@@ -1,11 +1,13 @@
-import { IsString, IsUrl } from 'class-validator';
+import { IsNotEmpty, IsString, IsUrl } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 
 export class CreateBrandDto {
   @IsString()
+  @IsNotEmpty()
   readonly name: string;
 
   @IsUrl()
+  @IsNotEmpty()
   readonly image: string;
 }
 
--- src/dtos/categories.dto.ts
+++ src/dtos/categories.dto.ts
@@ -1,8 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateCategoryDto {
   @IsString()
+  @IsNotEmpty()
   readonly name: string;
 }
 
