e013e128c37b2c4c052d5ae56427ce74210904c3
David Merchan <juan1014166@gmail.com>
2021-09-10 09:55:39 -0500

    add generateJWT

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,19 +1,32 @@
 import { Injectable } from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { compare } from 'bcrypt';
 
 import { UsersService } from '../../users/services/users.service';
+import { User } from '../../users/entities/user.entity';
+import { PayloadToken } from '../models/token.model';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string) {
     const user = await this.usersService.findByEmail(email);
-    if (!user) return null;
 
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (isMatch) return user;
+    if (user && (await compare(password, user.password))) return user;
 
     return null;
+  }
+
+  async generateJWT(user: User) {
+    const payload: PayloadToken = { role: user.role, sub: user.id };
+
+    return {
+      acces_token: this.jwtService.sign(payload),
+      user,
+    };
   }
 }
