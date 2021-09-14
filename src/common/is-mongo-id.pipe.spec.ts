c59668a297e8758df149a911e7ec6bea8e0788c5
David Merchan <juan1014166@gmail.com>
2021-09-14 09:41:10 -0500

    delete spec files

--- src/common/is-mongo-id.pipe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { IsMongoIdPipe } from './is-mongo-id.pipe';
-
-describe('IsMongoIdPipe', () => {
-  it('should be defined', () => {
-    expect(new IsMongoIdPipe()).toBeDefined();
-  });
-});
