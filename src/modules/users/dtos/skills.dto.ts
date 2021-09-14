9acc0b72cabc9029d448babb5494e5ab35a2fac6
David Merchan <juan1014166@gmail.com>
2021-09-14 14:26:36 -0500

    docs skills

--- src/modules/users/dtos/skills.dto.ts
+++ src/modules/users/dtos/skills.dto.ts
@@ -1,13 +1,15 @@
 import { IsNotEmpty, IsString } from 'class-validator';
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 
 export class CreateSkillDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({ description: 'Skill name' })
   readonly name: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({ description: 'Skill color' })
   readonly color: string;
 }
 
