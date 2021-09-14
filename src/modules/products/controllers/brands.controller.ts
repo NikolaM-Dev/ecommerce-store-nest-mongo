8c411e95189d763f71bade81c89c6d92f0fd094c
David Merchan <juan1014166@gmail.com>
2021-09-14 11:23:41 -0500

    docs brands

--- src/modules/products/controllers/brands.controller.ts
+++ src/modules/products/controllers/brands.controller.ts
@@ -9,34 +9,38 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
 import { BrandsService } from '../services/brands.service';
 import { CreateBrandDto, UpdateBrandDto } from '../dtos/brands.dto';
 import { IsMongoIdPipe } from '../../../common/is-mongo-id.pipe';
 
-@ApiTags('brands')
+@ApiTags('Brands')
 @Controller('brands')
 export class BrandsController {
   constructor(private brandsService: BrandsService) {}
 
   @Get()
+  @ApiOperation({ summary: 'List of brands' })
   findMany() {
     return this.brandsService.findMany();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Find a brand  by ID' })
   @HttpCode(HttpStatus.ACCEPTED)
   async findById(@Param('id', IsMongoIdPipe) id: string) {
     return await this.brandsService.findById(id);
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create a brand' })
   create(@Body() payload: CreateBrandDto) {
     return this.brandsService.create(payload);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Update a brand by ID' })
   async update(
     @Param('id', IsMongoIdPipe) id: string,
     @Body() payload: UpdateBrandDto,
@@ -45,6 +49,7 @@ export class BrandsController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Remove a brand by ID' })
   async remove(@Param('id', IsMongoIdPipe) id: string) {
     return await this.brandsService.remove(id);
   }
