e817a9a6e8fb3bf97228863233a3b68bc4cea338
David Merchan <juan1014166@gmail.com>
2021-09-14 14:24:46 -0500

    docs users

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -9,40 +9,45 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { CreateUserDto, UpdateUserDto } from '../dtos/users.dto';
 import { UsersService } from '../services/users.service';
 import { IsMongoIdPipe } from '../../../common/is-mongo-id.pipe';
 
-@ApiTags('users')
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Get()
+  @ApiOperation({ summary: 'List of users' })
   findMany() {
     return this.usersService.findMany();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Find a user by ID' })
   @HttpCode(HttpStatus.ACCEPTED)
   async findById(@Param('id', IsMongoIdPipe) id: string) {
     return await this.usersService.findById(id);
   }
 
   @Get(':id/orders')
+  @ApiOperation({ summary: 'List all user orders with ID' })
   @HttpCode(HttpStatus.ACCEPTED)
   async findOrderByUserId(@Param('id', IsMongoIdPipe) id: string) {
     return await this.usersService.findOrderByUserId(id);
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create a user' })
   async create(@Body() payload: CreateUserDto) {
     return await this.usersService.create(payload);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Update a user by ID' })
   async update(
     @Param('id', IsMongoIdPipe) id: string,
     @Body() payload: UpdateUserDto,
@@ -51,6 +56,7 @@ export class UsersController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Remove a user by ID' })
   async remove(@Param('id', IsMongoIdPipe) id: string) {
     return await this.usersService.remove(id);
   }
--- src/modules/users/dtos/users.dto.ts
+++ src/modules/users/dtos/users.dto.ts
@@ -4,16 +4,18 @@ import { PartialType, ApiProperty } from '@nestjs/swagger';
 export class CreateUserDto {
   @IsEmail()
   @IsNotEmpty()
-  @ApiProperty({ description: "The user's email" })
+  @ApiProperty({ description: 'User email' })
   readonly email: string;
 
   @IsString()
   @Length(6)
   @IsNotEmpty()
+  @ApiProperty({ description: 'User password' })
   readonly password: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ description: 'User role', enum: ['admin, customer'] })
   readonly role: string;
 }
 
