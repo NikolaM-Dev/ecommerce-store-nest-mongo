ac24705b206ef4c51595c76b20a1ae46f484ac05
David Merchan <juan1014166@gmail.com>
2021-09-14 14:23:14 -0500

    docs root

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,24 +3,29 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Public } from './auth/decorators/public.decorator';
 import { ApiKeyGuard } from './auth/guards/api-key.guard';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Root')
 @UseGuards(ApiKeyGuard)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Hello World!' })
   @Public()
   getHello(): string {
     return this.appService.getHello();
   }
 
   @Get('new')
+  @ApiOperation({ summary: 'Return string "Hi i\'m new"' })
   new() {
     return "Hi i'm new";
   }
 
   @Get('tasks')
+  @ApiOperation({ summary: 'List of tasks' })
   async findManyTasks() {
     return await this.appService.findManyTasks();
   }
