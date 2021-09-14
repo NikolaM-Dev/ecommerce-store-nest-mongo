64ab8e511bea87b2606340e59257047888c2021b
David Merchan <juan1014166@gmail.com>
2021-09-14 14:24:21 -0500

    docs profile

--- src/modules/users/controllers/profile.controller.ts
+++ src/modules/users/controllers/profile.controller.ts
@@ -1,4 +1,6 @@
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { Controller, Req, Get, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
 
 import { Role } from 'src/auth/models/role.model';
 import { Roles } from '../../../auth/decorators/roles.decorator';
@@ -6,8 +8,8 @@ import { PayloadToken } from '../../../auth/models/token.model';
 import { JwtAuthGuard } from '../../../auth/guards/jwt-auth.guard';
 import { RolesGuard } from '../../../auth/guards/roles.guard';
 import { OrdersService } from '../services/orders.service';
-import { Request } from 'express';
 
+@ApiTags('Profile')
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('profile')
 export class ProfileController {
@@ -15,6 +17,7 @@ export class ProfileController {
 
   @Roles(Role.CUSTOMER)
   @Get('my-orders')
+  @ApiOperation({ summary: 'Customer order list' })
   findManyOrders(@Req() req: Request) {
     const user = req.user as PayloadToken;
 
