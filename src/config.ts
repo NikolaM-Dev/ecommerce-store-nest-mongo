d2180fc845c439758cb0d5d6d2437e9c1a55f73c
David Merchan <juan1014166@gmail.com>
2021-09-17 15:38:03 -0500

    add config enviroment variables

--- src/config.ts
+++ src/config.ts
@@ -6,4 +6,11 @@ export default registerAs('config', () => ({
     port: process.env.DATABASE_PORT,
   },
   apiKey: process.env.API_KEY,
+  postgres: {
+    dbName: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    port: parseInt(process.env.POSTGRES_PORT, 10),
+    host: process.env.POSTGRES_HOST,
+  },
 }));
