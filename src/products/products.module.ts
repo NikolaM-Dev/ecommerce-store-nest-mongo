0143268324b7207d7c772c3f79ae997ca50fec91
David Merchan <juan1014166@gmail.com>
2021-09-20 11:59:03 -0500

    feat: :construction: add product entity

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { BrandsController } from './controllers/brands.controller';
-import { CategoriesController } from './controllers/categories.controller';
-import { ProductsController } from './controllers/products.controller';
 import { BrandsService } from './services/brands.service';
+import { CategoriesController } from './controllers/categories.controller';
 import { CategoriesService } from './services/categories.service';
+import { Product } from 'src/products/entities/product.entity';
+import { ProductsController } from './controllers/products.controller';
 import { ProductsService } from './services/products.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product])],
   controllers: [ProductsController, CategoriesController, BrandsController],
   providers: [ProductsService, BrandsService, CategoriesService],
   exports: [ProductsService],
