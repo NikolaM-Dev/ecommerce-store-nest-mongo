b5ef552c4d81c72a2856346a505b921e9e601748
David Merchan <juan1014166@gmail.com>
2021-09-23 14:36:23 -0500

    feat(brands): add typing to the customer entity

--- src/products/entities/brand.entity.ts
+++ src/products/entities/brand.entity.ts
@@ -1,5 +1,39 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { Product } from './product.entity';
+
+@Entity()
 export class Brand {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 255, unique: true })
   name: string;
+
+  @Column({ type: 'varchar', length: 255 })
   image: string;
+
+  @OneToMany(() => Product, (product) => product.brand)
+  products: Product[];
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
 }
