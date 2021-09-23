ffd3109bad74c79f511d02120e7f67e3af5f14ce
David Merchan <juan1014166@gmail.com>
2021-09-22 20:06:03 -0500

    feat(customers): add typing to the customer entity

--- src/users/entities/customer.entity.ts
+++ src/users/entities/customer.entity.ts
@@ -1,6 +1,42 @@
+import {
+  CreateDateColumn,
+  UpdateDateColumn,
+  Column,
+  OneToOne,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { User } from './user.entity';
+
+@Entity()
 export class Customer {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 255 })
   name: string;
+
+  @Column({ type: 'varchar', length: 255, name: 'last_name' })
   lastName: string;
+
+  @Column({ type: 'varchar', length: 255 })
   phone: string;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+
+  @OneToOne(() => User, (user) => user.customer, { nullable: true })
+  user: User;
 }
