88869645c142e9e3752a73166343183cd05c1204
David Merchan <juan1014166@gmail.com>
2021-10-06 16:04:45 -0500

    docs(categories): auto documentation with swagger

--- src/products/dtos/categories.dto.ts
+++ src/products/dtos/categories.dto.ts
@@ -1,9 +1,10 @@
 import { IsNotEmpty, IsString } from 'class-validator';
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 
 export class CreateCategoryDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   readonly name: string;
 }
 
